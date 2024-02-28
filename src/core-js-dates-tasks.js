8a8c27d44c52e8f9bc2869594bce8347c4d7184f
Luba Hadunova <luba.godunova2018@gmail.com>
2024-02-28 20:50:51 +0100

    feat: add solution for formatDate task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -148,8 +148,13 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dayData = new Date(date);
+  const month = dayData.getUTCMonth() + 1;
+  const day = dayData.getUTCDate();
+  const year = dayData.getUTCFullYear();
+  const time = dayData.toLocaleTimeString('en-US', { timeZone: 'UTC' });
+  return `${month}/${day}/${year}, ${time}`;
 }
 
 /**
